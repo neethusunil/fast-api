f69ee02bd000b4530be3546dcc3f7ccedd95e437
Neethu Sunil <neethu@toasoft.onmicrosoft.com>
2022-08-04 18:05:25 +0900

    imnventory example with path and query parameters

--- working.py
+++ working.py
@@ -1,13 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Optional
 
 #create an object
 app = FastAPI() 
 
-#create an endpoint
-@app.get("/")
-def home():
-    return {"Data": "Testing"}
+inventory = {
+    1: {
+        "name": "Milk",
+        "price": 3.99,
+        "brand": "Milma"
+    }
+}
 
-@app.get("/about")
-def about():
-    return{"Data": "About"}
+#path parameters
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int = Path(None, description="The ID of the item that you would like to view", gt=0, lt=2)):
+    return inventory[item_id]
+
+#query parameters
+@app.get("/get-by-name")
+def get_item(test: int,name: Optional[str] = None ):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return inventory[item_id]
+    return {"Data": "Not found"}
