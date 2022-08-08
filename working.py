23bcba19e3bcff6d204179c79e8f1480c70e3ac4
Neethu Sunil <neethu@toasoft.onmicrosoft.com>
2022-08-08 10:00:49 +0900

    request body and post method

--- working.py
+++ working.py
@@ -11,28 +11,28 @@ class Item(BaseModel):
     brand: Optional[str] = None
 
 inventory = {
-    1: {
-        "name": "Milk",
-        "price": 3.99,
-        "brand": "Milma"
-    }
+    
 }
 
 #path parameters
 @app.get("/get-item/{item_id}")
-def get_item(item_id: int = Path(None, description="The ID of the item that you would like to view", gt=0, lt=2)):
+def get_item(item_id: int = Path(None, description="The ID of the item that you would like to view")):
     return inventory[item_id]
 
 #query parameters
 @app.get("/get-by-name")
 def get_item(test: int,name: Optional[str] = None ):
     for item_id in inventory:
-        if inventory[item_id]["name"] == name:
+        if inventory[item_id].name == name:
             return inventory[item_id]
     return {"Data": "Not found"}
 
 
 #request body
-@app.post("/create-item")
-def create_item(item: Item):
-    return{}
+@app.post("/create-item/{item-id}")
+def create_item(item_id: int, item: Item):
+    if item_id in inventory:
+        return {"Error": "Item ID already exists."}
+    inventory[item_id] = item
+    return inventory[item_id]
+    
